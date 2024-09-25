90152c39feb85efa30ee185907fce78bada666db
Mustafa Şahin <mustafa@192.168.1.34>
2024-09-25 20:05:39 +0300

    First commit

---
+++ App.jsx
@@ -0,0 +1,16 @@
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+
+//navigation import
+import Router from './src/navigation/Router';
+
+
+const App = () => {
+  return (
+<NavigationContainer>
+  <Router/>
+</NavigationContainer>
+  );
+};
+
+export default App;
